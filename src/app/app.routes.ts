eab461388e4637b22c193c9286da384420b7f840
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2024-11-06 21:22:01 +0200

    Initial commit - setup Angular project structure and basic components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'catalog', component: CatalogComponent },
+    { path: 'details/:id', component: DetailsComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
